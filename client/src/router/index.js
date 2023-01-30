c38218d50c689d73c27af5be4ad962479b18b41f
enes taha sarı <ets.34.es@gmail.com>
2023-01-30 22:16:01 +0300

    web hash history

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import store from "../store/index";
 import { isObject } from "util";
 
@@ -97,7 +97,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
   scrollBehavior(to, from, savedPosition) {
     // always scroll to top
